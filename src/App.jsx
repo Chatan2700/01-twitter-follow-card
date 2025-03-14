f1bc9946c2aafa9017b9684dfead01b2d0b27dff
Sebastian Rios <juanriosubeda@gmail.com>
2025-03-13 21:56:18 -0700

    component-styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import './index.css'
 
 function App() {
 
   return (
     <>
-  
+      <article>
+        <header>
+          <img src="https://pbs.twimg.com/profile_images/1696953461412442112/nMjG1ZdU_400x400.jpg" alt="Dribble avatar" />
+          <div>
+            <strong>Dribbble</strong>
+            <span>@dribbble</span>
+          </div>
+        </header>
+
+        <aside>
+          <button>Follow</button>
+        </aside>
+      </article>
     </>
   )
 }
